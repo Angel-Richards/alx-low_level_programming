4beea012da7190669514e3f1ff698daddbc0e6ff
Angel-Richards <missangelrichards@gmail.com>
2023-03-03 16:01:26 -0800

    correct 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,16 +8,16 @@
   */
 void print_number(int n)
 {
-unsigned int x;
+	unsigned int x;
 
-if (n < 0)
-{
-_putchar('-');
-n *= -1;
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
 
-x = n;
-if (x / 10)
-	print_number(x / 10);
-	_putchar(x  %  10  +  '0');
+	x = n;
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10  +  '0');
 }
