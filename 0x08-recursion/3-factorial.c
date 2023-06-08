b02885bf6fa6e7e6ed75a742e05c668be9349cd9
Angel-Richards <missangelrichards@gmail.com>
2023-06-08 02:24:01 -0700

    update

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,19 +7,18 @@
 int factorial(int n)
 {
 	int d;
-	{
+
 	if (n == 0)
+	{
 		return (1);
 	}
-	{
 	else if (n < 0)
+	{
 		return (-1);
 	}
-	{
 	else
 	{
-		x = n * factorial(n - 1);
+		d = n * factorial(n - 1);
 	}
 	return (d);
-	}
 }
