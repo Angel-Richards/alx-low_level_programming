08155242f293b78ee0da32efb588a3bde9cd9999
Angel-Richards <missangelrichards@gmail.com>
2023-06-18 13:23:50 -0700

    update

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int d;
+	int f;
+
+	d = 0;
+	while (dest[d] != '\0')
+	{
+		d++;
+	}
+	f = 0;
+	while (f < n && src[f] != '\0')
+	{
+		dest[d] = src[f];
+		d++;
+		f++;
+	}
+	dest[d] '\0';
+	return (dest);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*_puts - outputs a string*/
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
