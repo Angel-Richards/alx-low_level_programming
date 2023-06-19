65ad9093c27b184dc55c20956ed04ca0eff92581
Angel-Richards <missangelrichards@gmail.com>
2023-06-19 14:20:50 -0700

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints number of arguments
+ * @argc: size of argv
+ * @argv: string of arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
