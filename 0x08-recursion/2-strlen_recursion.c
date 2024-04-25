a4a27476e73a40f99935b02732218d62e7163adb
Angel-Richards <missangelrichards@gmail.com>
2024-04-25 06:25:45 -0700

    commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: string to measure
+ * return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int i=0;
+
+	if(*s)
+	{
+		i++;
+		i+=_strlen_recursion(s + 1);
+	}
+
+	return (i);
+}
