97c83ce13a467c65b03dc349fe1047a5b230e1b1
Angel-Richards <missangelrichards@gmail.com>
2024-05-07 07:33:03 -0700

    commit

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit (0 to 9)
+ * @c: int to check
+ * Return: 1 if c is a digit, else return 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
